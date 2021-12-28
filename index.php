1bacde2c53e6bf002bd61be386609078df2df350
Knrc <kontakt.mikey@gmail.com>
2021-12-28 10:20:33 +0100

    Main Page initial commit

--- index.php
+++ index.php
@@ -1,41 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <!--Meta and OG-->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kappaarmy</title>
-    <meta name="description" content="Willkommen bei der KappaArmy! Wir nehmen jegliche art von hüter auf!">
-    <meta name="theme-color" content="#BEEB9F">
-    <link rel="shortcut icon" type="image/png" href="pictures/..."/>
+<?php
+include("includes/header.php");
+?>
+<!--Mendantory Header stuff-->
+  <?php
+  include("includes/Metadata.php");
+  ?>
 
-    <meta property="og:title" content="KappaArmy Clan">
-    <meta property="og:site_name" content="KappaArmy.com">
-    <meta property="og:url" content="">
-    <meta property="og:description" content="This is the gateway to the KappaArmy page, You'll find there everything about him :>">
-    <meta property="og:type" content="website">
-    <meta property="og:image" content="https://i.imgur.com/FmUu7iM.png">
+<link rel="shortcut icon" type="image/png" href="pictures/..."/>
+    
+<!-- Styles-->
+  <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 
-    <!-- Twitter Card-->
-    <!--<meta name="twitter:card" content="Knrc's Webpage">
-    <meta name="twitter:creator" content="@Knrcart">
-    <meta name="twitter:title" content="Knrc.art">
-    <meta name="twitter:description" content="This is the gateway to Knrc's Page, You'll find there everything about him :>">
-    <meta name="twitter:image" content="https://i.imgur.com/FmUu7iM.png">
-    <meta name="twitter:image:alt" content="Knrcs OC character">-->
-
-    <!--Fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet">
-    <!-- Styles-->
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-
-  </head>
+</head>
 
 <body>
-Bruh KappaArmy frontpage
-</body>
 
-</html>
+<div class="the-left-box sticky">
+  <div class="content">
+    <h1> Discord bruh </h1>
+    <p> Discord shtuff </p>
+  </div>
+</div>
+
+<div class="the-main-box">
+  <div class="content">
+    <h1> Introduction and shit </h1>
+    <p> Hi whats up KappaArmy here </p>
+  </div>
+</div>
+
+<div class="the-right-box sticky">
+  <div class="content">
+    <h1> Who's online and shit </h1>
+    
+    
+    
+  </div>
+</div>
+
+
+
+<?php
+include("includes/footer.php");
+?>
