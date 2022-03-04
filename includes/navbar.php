65eff0afd1676ee15de68081483ca760e78f50ae
Knrc <kontakt.mikey@gmail.com>
2022-03-04 04:46:31 +0100

    Added Navbar and new sections
    + Navbar
    + Sections
    + adepted the onlinenum script

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,29 +1,52 @@
 <header>
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-    <div class="container-fluid">
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-mdb-toggle="collapse"
-        data-mdb-target="#navbarExample01"
-        aria-controls="navbarExample01"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarExample01">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item active">
-            <a class="nav-link" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Clan Members</a>
-          </li>
-        </ul>
-      </div>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: rgba(0, 0, 0, 0.2);">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="index.php">HOME</a>
+
+    <!-- Toggle button -->
+    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <i class="fas fa-bars"></i>
+    </button>
+
+    <!-- Collapsible wrapper -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <!-- Link -->
+        <li class="nav-item">
+          <a class="nav-link" href="clan.php">Memberlist</a>
+        </li>
+
+        <!-- Dropdown -->
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a> -->
+          <!-- Dropdown menu -->
+          <!-- <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li>
+              <a class="dropdown-item" href="#">Action</a>
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Another action</a>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <li>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </li>
+          </ul>
+        </li> -->
+
+      </ul>
+
     </div>
-  </nav>
-  <!-- Navbar -->
-</header>
+  </div>
+  <!-- Container wrapper -->
+</nav>
+<!-- Navbar -->
--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   </head>
   <body>
     <!-- Start your project here-->
+    <?php include("includes/navbar.php");?>
 
 
 
@@ -19,10 +20,18 @@
         <h1>Introduction</h1>
         <p>Heya, wie ihr seht, könnt ihr alle Clanmitglieder sehen, die gerade online sind in Destiny und auf welcher Plattform sie spielen. Ich hoffe, dass es euch dabei hilft zu sehen, wer gerade online ist in Destiny. Die Seite wird definitiv noch angepasst, das ist derzeit nur ein Prototype für die Funktionalität</p>
         </br> <!-- Lazy but effective strat idgaf-->
+        <h1>Discord</h1>
+        <p>Wir nutzen den Discord als hauptsächliches Kommunikations mittel, damit wir uns Clan und Plattform übergreifend unterhalten können! Wir empfehlen euch unseren Discord beizutreten, wenn ihr an Community Events teilnehmen wollt und schnell nach Leuten sucht, die mit euch Aktivitäten erledigen. </p>
+        <a href="https://discord.gg/Pyn2AYH5Wn"><button type="button" class="btn btn-primary btn-rounded" style="background-color: #7289da">Discord</button></a>
+        </br>
         </br>
         <h1>Support</h1>
-        <p>Falls ihr meine Projekte unterstützen wollt, könnt ihr es gerne auf Patreon machen! Es hilft mehr sehr den Server online zu halten ^^</p>
+        <p>Falls ihr meine Projekte unterstützen wollt, könnt ihr es gerne auf Patreon machen! Es hilft mir sehr, die Domain und die Website online zu halten und weiter an diesem Projekt zu arbeiten :D</p>
         <a href="https://patreon.com/KappaArmy"><button type="button" class="btn btn-primary btn-rounded" style="background-color: #FF424D">Patreon</button></a>
+        </br>
+        </br>
+        <p> Aktive Supporter dieser Website: </p>
+        </br>
         </br>
       </div>
 
@@ -31,7 +40,7 @@
       </br>
         <h1> Spieler, die gerade Online sind </h1>
         <?php include("includes/Destiny_2/onlinenum.php");
-        echo "Insgesamt sind ". $i ." online!"; ?>
+        echo "Insgesamt sind ". $online ." online!"; ?>
         <?php include("includes/Destiny_2/onlinemembers.php"); ?>
       </div>
 
@@ -39,7 +48,7 @@
 </div>
 </div>
 
-<?php include("includes/Discord/Clanspace_check.php"); ?>
+<?php //include("includes/Discord/Clanspace_check.php"); ?>
 
 
     <!-- End your project here-->
